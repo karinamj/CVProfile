39fe5355f66bdb8065d6cae8d8eb85d332d2447f
jmacabrera <jmacabrera@poligran.edu.co>
2025-09-22 21:22:49 -0500

    codigo java de  variables

---
+++ ResumenEstructuraProyecto.java
@@ -0,0 +1,127 @@
+// Resumen estructural del proyecto CVProfile en formato Java
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\MainActivity.kt
+public class MainActivity {
+    // Métodos detectados
+    // override Método detectado
+    void onCreate(savedInstanceState: Bundle?) { ... }
+    // override Método detectado
+    void onCreateOptionsMenu(menu: Menu) { ... }
+    // override Método detectado
+    void onSupportNavigateUp() { ... }
+    // Listeners detectados
+    // Listener detectado en línea 29: binding.appBarMain.fab.setOnClickListener { view ->
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\button\ButtonFragment.kt
+public class ButtonFragment {
+    // Métodos detectados
+    // Método detectado
+    void newInstance() { ... }
+    // override Método detectado
+    void onCreate(savedInstanceState: Bundle?) { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\button\ButtonViewModel.kt
+public class ButtonViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\gallery\GalleryFragment.kt
+public class GalleryFragment {
+    // Métodos detectados
+    // override Método detectado
+    void onDestroyView() { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\gallery\GalleryViewModel.kt
+public class GalleryViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\home\HomeFragment.kt
+public class HomeFragment {
+    // Métodos detectados
+    // override Método detectado
+    void onDestroyView() { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\home\HomeViewModel.kt
+public class HomeViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\photo\PhotoFragment.kt
+public class PhotoFragment {
+    // Métodos detectados
+    // Método detectado
+    void newInstance() { ... }
+    // override Método detectado
+    void onCreate(savedInstanceState: Bundle?) { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\photo\PhotoViewModel.kt
+public class PhotoViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\profile\ProfileFragment.kt
+public class ProfileFragment {
+    // Métodos detectados
+    // override Método detectado
+    void onDestroyView() { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\profile\ProfileViewModel.kt
+public class ProfileViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\slideshow\SlideshowFragment.kt
+public class SlideshowFragment {
+    // Métodos detectados
+    // override Método detectado
+    void onDestroyView() { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\slideshow\SlideshowViewModel.kt
+public class SlideshowViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\video\VideoFragment.kt
+public class VideoFragment {
+    // Métodos detectados
+    // Método detectado
+    void newInstance() { ... }
+    // override Método detectado
+    void onCreate(savedInstanceState: Bundle?) { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\video\VideoViewModel.kt
+public class VideoViewModel {
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\web\WebFragment.kt
+public class WebFragment {
+    // Métodos detectados
+    // Método detectado
+    void newInstance() { ... }
+    // override Método detectado
+    void onCreate(savedInstanceState: Bundle?) { ... }
+}
+
+
+// Archivo: ./app/src/main/java\com\example\cvprofile\ui\web\WebViewModel.kt
+public class WebViewModel {
+}
