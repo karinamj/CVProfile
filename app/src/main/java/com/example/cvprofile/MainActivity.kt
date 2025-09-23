f308486e66cbb04eee45ed158431d0da11e9f078
Lenovo <lizethmartinez404@gmail.com>
2025-09-22 23:46:02 -0500

    Refactor menú lateral: botones ahora muestran icono y texto dentro, todos los botones usan drawableTop y fondo cuadrado con bordes suaves

--- app/src/main/java/com/example/cvprofile/MainActivity.kt
+++ app/src/main/java/com/example/cvprofile/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.cvprofile
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageButton
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,7 +13,13 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.cvprofile.databinding.ActivityMainBinding
+import com.example.cvprofile.ui.button.ButtonFragment
+import com.example.cvprofile.ui.photo.PhotoFragment
+import com.example.cvprofile.ui.profile.ProfileFragment
+import com.example.cvprofile.ui.video.VideoFragment
+import com.example.cvprofile.ui.web.WebFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,24 +28,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // Botones del menú lateral
+        // Botones del menú lateral
+        findViewById<TextView>(R.id.btnPerfil).setOnClickListener {
+            abrirFragment(ProfileFragment())
+        }
+        findViewById<TextView>(R.id.btnFotos).setOnClickListener {
+            abrirFragment(PhotoFragment())
+        }
+        findViewById<TextView>(R.id.btnVideo).setOnClickListener {
+            abrirFragment(VideoFragment())
+        }
+        findViewById<TextView>(R.id.btnWeb).setOnClickListener {
+            abrirFragment(WebFragment())
+        }
+        findViewById<TextView>(R.id.btnBotones).setOnClickListener {
+            abrirFragment(ButtonFragment())
+        }
 
-        setSupportActionBar(binding.appBarMain.toolbar)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_profile, R.id.nav_photo, R.id.nav_video, R.id.nav_web, R.id.nav_button
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        // Fragmento inicial
+        if (savedInstanceState == null) {
+            abrirFragment(ProfileFragment())
+        }
+    }
+
+    private fun abrirFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.contenedorFragmentos, fragment)
+            .commit()
     }
 
     override fun onSupportNavigateUp(): Boolean {
